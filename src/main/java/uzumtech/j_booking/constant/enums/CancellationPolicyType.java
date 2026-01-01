fc9deef277fbdd02c443125cb7cd9b422ce9fd3a
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2026-01-01 19:33:13 +0500

    create CancellationPolicyType record

---
+++ src/main/java/uzumtech/j_booking/constant/enums/CancellationPolicyType.java
@@ -0,0 +1,7 @@
+package uzumtech.j_booking.constant.enums;
+
+public enum CancellationPolicyType {
+    FREE_CANCELLATION,    // Бесплатная отмена
+    NON_REFUNDABLE,       // Безвозвратный тариф
+    PARTIAL_REFUND        // Частичный возврат (например, штраф за 1 сутки)
+}
