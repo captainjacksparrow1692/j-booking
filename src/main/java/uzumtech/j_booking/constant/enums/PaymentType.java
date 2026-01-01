c002581a98893d0f083dae1e3043a5ec2e7e91f8
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2026-01-01 19:34:17 +0500

    create PaymentType record

---
+++ src/main/java/uzumtech/j_booking/constant/enums/PaymentType.java
@@ -0,0 +1,7 @@
+package uzumtech.j_booking.constant.enums;
+
+public enum PaymentType {
+    PREPAID,              // Полная предоплата
+    POSTPAID,             // Оплата на месте (в отеле)
+    PARTIAL               // Частичная предоплата (депозит)
+}
