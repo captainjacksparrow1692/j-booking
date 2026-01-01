4af0143f4e298051295a90796f6355d54fb0d5f3
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2026-01-01 19:28:52 +0500

    create BoardBasis record

---
+++ src/main/java/uzumtech/j_booking/constant/enums/BoardBasis.java
@@ -0,0 +1,9 @@
+package uzumtech.j_booking.constant.enums;
+
+public enum BoardBasis {
+    RO,  // Room Only (без питания)
+    BB,  // Bed & Breakfast (завтрак)
+    HB,  // Half Board (полупансион: завтрак + ужин)
+    FB,  // Full Board (полный пансион)
+    AI   // All Inclusive (все включено)
+}
