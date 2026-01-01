97e96d03accb7a755595d8d7c30bdf6250665a8b
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2026-01-02 02:54:40 +0500

    create HotelDetailResponse record

--- src/main/java/uzumtech/j_booking/dto/response/HotelDetailResponse.java
+++ src/main/java/uzumtech/j_booking/dto/response/HotelDetailResponse.java
@@ -4,6 +4,7 @@ import uzumtech.j_booking.constant.enums.AccommodationType;
 import uzumtech.j_booking.constant.enums.Amenity;
 import uzumtech.j_booking.dto.response.RoomResponse;
 
+import java.util.List;
 import java.util.Set;
 
 public record HotelDetailResponse (
