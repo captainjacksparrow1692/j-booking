5a0a14c232ad0a27efd153b46b008e943acc38e9
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2026-01-02 23:43:04 +0500

    update BookingService

--- src/main/java/uzumtech/j_booking/service/BookingService.java
+++ src/main/java/uzumtech/j_booking/service/BookingService.java
@@ -1,5 +1,7 @@
 package uzumtech.j_booking.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import uzumtech.j_booking.dto.request.BookingCreateRequest;
 import uzumtech.j_booking.dto.response.BookingResponse;
 
@@ -9,4 +11,5 @@ public interface BookingService {
     BookingResponse createBooking(BookingCreateRequest dto);
     BookingResponse getBookingById(Long id);
     List<BookingResponse> getUserBookings(Long userId);
+    Page<BookingResponse> getUserBookings(Long userId, Pageable pageable);
 }
