a7d010341613c853abf168d24fbfcca92380fba6
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2026-01-02 22:58:36 +0500

    create DataSourceConfiguration record

---
+++ src/main/java/uzumtech/j_booking/configuration/DataSourceConfiguration.java
@@ -0,0 +1,13 @@
+package uzumtech.j_booking.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableJpaAuditing
+@EnableTransactionManagement
+@EnableJpaRepositories("uzumtech.j_booking.repository")
+public class DataSourceConfiguration {
+}
