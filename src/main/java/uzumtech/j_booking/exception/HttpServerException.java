075222f821539a2bbf38bd0624944c6ab8caefaa
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2026-01-07 02:25:24 +0500

    create HttpServerException record

---
+++ src/main/java/uzumtech/j_booking/exception/HttpServerException.java
@@ -0,0 +1,16 @@
+package uzumtech.j_booking.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+import lombok.Getter;
+import uzumtech.j_booking.constant.enums.ErrorType;
+
+import static uzumtech.j_booking.constant.enums.Error.HTTP_SERVICE_ERROR_CODE;
+
+@Getter
+public class HttpServerException extends BussinesException {
+    public HttpServerException(String message, HttpStatusCode status) {
+        super(HTTP_SERVICE_ERROR_CODE.getCode(), message, ErrorType.EXTERNAL, HttpStatus.valueOf(status.value()));
+    }
+}
