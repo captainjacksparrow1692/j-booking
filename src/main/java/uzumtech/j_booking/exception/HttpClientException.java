5b54fac137f30547f3dbb033d473bc5162d46962
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2026-01-07 02:21:25 +0500

    create HttpClientException record

---
+++ src/main/java/uzumtech/j_booking/exception/HttpClientException.java
@@ -0,0 +1,15 @@
+package uzumtech.j_booking.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.HttpStatus;
+import uzumtech.j_booking.constant.enums.ErrorType;
+
+import static uzumtech.j_booking.constant.enums.Error.HTTP_CLIENT_ERROR_CODE;
+
+@Getter
+public class HttpClientException extends BussinesException{
+    public HttpClientException(String message, HttpStatusCode status) {
+        super(HTTP_CLIENT_ERROR_CODE.getCode(), message, ErrorType.EXTERNAL, HttpStatus.valueOf(status.value()));
+    }
+}
