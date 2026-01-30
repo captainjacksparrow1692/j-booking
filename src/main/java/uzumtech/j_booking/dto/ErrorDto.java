1b00d17e4e88b2c46fa15d2a3d9d8580e8b0ff5d
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2026-01-30 17:05:18 +0500

    update

--- src/main/java/uzumtech/j_booking/dto/ErrorDto.java
+++ src/main/java/uzumtech/j_booking/dto/ErrorDto.java
@@ -1,14 +1,17 @@
 package uzumtech.j_booking.dto;
 
 import lombok.Builder;
+import uzumtech.j_booking.constant.enums.ErrorType;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Builder
 public record ErrorDto(
         int code,
         String message,
-        String errorCode,
-        LocalDateTime timestamp
+        ErrorType type,
+        LocalDateTime timestamp,
+        List<String> validationErrors
 ) {
 }
