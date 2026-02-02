48e0a76732b0466572d66a236e98d7621caf6127
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2026-02-02 17:19:45 +0500

    update

--- src/main/java/uzumtech/j_booking/configuration/kafka/ProducerConfiguration.java
+++ src/main/java/uzumtech/j_booking/configuration/kafka/ProducerConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.kafka.support.serializer.JacksonJsonSerializer;
 import uzumtech.j_booking.configuration.props.KafkaProps;
 import uzumtech.j_booking.dto.response.BookingResponse;
 import uzumtech.j_booking.dto.DlqDto;
@@ -43,7 +43,7 @@ public class ProducerConfiguration {
 
         // Сериализаторы Kafka
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JacksonJsonSerializer.class);
 
         return props;
     }
